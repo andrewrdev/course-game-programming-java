b82050710d013276bbbe0e6b06e460a5d83131d9
andrewrdev <84985099+andrewrdev@users.noreply.github.com>
2023-09-25 23:00:09 -0300

    Episode 4 - Window

--- src/andrewrdev/rain/Game.java
+++ src/andrewrdev/rain/Game.java
@@ -1,13 +1,26 @@
 package andrewrdev.rain;
 
-public class Game implements Runnable {
+import java.awt.Canvas;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+	
+	private static final long serialVersionUID = 1L;
 	
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
 	
 	private Thread thread;
+	private JFrame frame;
 	private boolean running = false;
+	
+	public Game(){
+		Dimension size = new Dimension(width * scale, height * scale);
+		setPreferredSize(size);
+		frame = new JFrame();
+	}
 	
 	public synchronized void start() {
 		running = true;
@@ -25,9 +38,22 @@ public class Game implements Runnable {
 	}
 
 	public void run() {
+				
 		while (running){
-			
+			System.out.println("Running ...");
 		}
+	}
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+		
+		game.frame.setResizable(false);
+		game.frame.setTitle("Rain");
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.setVisible(true);
 	}
 
 }
