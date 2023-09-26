bb0b1f45f957963ec224f3734aafe29d9406607d
andrewrdev <84985099+andrewrdev@users.noreply.github.com>
2023-09-25 21:42:28 -0300

    Episode 2 - Threads

--- src/andrewrdev/rain/Game.java
+++ src/andrewrdev/rain/Game.java
@@ -1,9 +1,29 @@
 package andrewrdev.rain;
 
-public class Game {
+public class Game implements Runnable {
 	
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
+	
+	private Thread thread;
+	
+	public synchronized void start() {
+		thread = new Thread(this, "Display");
+		thread.start();
+	}
+	
+	public synchronized void stop() {
+		try {
+			thread.join();
+		} catch (InterruptedException e) {			
+			e.printStackTrace();
+		}
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
