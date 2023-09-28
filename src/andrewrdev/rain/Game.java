ad0170d44373938b827610a63c7766fd87609fae
andrewrdev <84985099+andrewrdev@users.noreply.github.com>
2023-09-28 19:24:00 -0300

    Episode 9 - Rendering Pixels

--- src/andrewrdev/rain/Game.java
+++ src/andrewrdev/rain/Game.java
@@ -71,11 +71,16 @@ public class Game extends Canvas implements Runnable {
 			createBufferStrategy(3);
 			return;
 		}
+
+		screen.render();
+
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = screen.pixels[i];
+		}
 		
-		Graphics g = bs.getDrawGraphics();
+		Graphics g = bs.getDrawGraphics();	
 		
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, getWidth(), getHeight());
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 		g.dispose();
 		bs.show();
 	}
--- src/andrewrdev/rain/graphics/Screen.java
+++ src/andrewrdev/rain/graphics/Screen.java
@@ -3,11 +3,19 @@ package andrewrdev.rain.graphics;
 public class Screen {
 	private int width;
 	private int height;
-	private int[] pixels;
-	
+	public int[] pixels;
+
 	public Screen(int width, int height) {
 		this.width = width;
 		this.height = height;
-		this.pixels = new int[width*height];
+		this.pixels = new int[width * height];
+	}
+
+	public void render() {
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				pixels[x + y * width] = 0xff00ff;
+			}
+		}
 	}
 }
